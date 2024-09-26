e5217be777f3e903716bfd2015f8112bd7e6bc03
Domat Al Khoury <domatalkhoury10@gmail.com>
2024-09-26 12:45:47 -0400

    Initial commit

---
+++ src/main/java/com/Lorem_Ipsum_SOEN343/Lorem_Ipsum_SOEN343/LoremIpsumSoen343Application.java
@@ -0,0 +1,13 @@
+package com.Lorem_Ipsum_SOEN343.Lorem_Ipsum_SOEN343;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoremIpsumSoen343Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoremIpsumSoen343Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lorem_Ipsum_SOEN343/Lorem_Ipsum_SOEN343/LoremIpsumSoen343ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lorem_Ipsum_SOEN343.Lorem_Ipsum_SOEN343;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoremIpsumSoen343ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
