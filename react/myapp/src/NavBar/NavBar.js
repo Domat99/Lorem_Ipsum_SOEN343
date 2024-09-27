2ce0566d30b18d62036c7f787d8e1b09e3cf212b
RawdaWaez <rawdawaez@gmail.com>
2024-09-26 21:00:16 -0400

    Home page update 1.0

--- react/myapp/src/NavBar/NavBar.js
+++ react/myapp/src/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import { menuNavbar } from "./MenuNavBar";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
@@ -6,17 +6,24 @@ import { Link } from "react-router-dom";
 class Navbar extends Component {
     state = { clicked: false };
 
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    };
+
     render() {
         return (
             <nav className="navbarList">
                 <a href="/">
-                    <img className="logo" src="" alt=" "/> {/* Add a logo source */}
+                    <img className="logo" src="/images/logo.png" alt="Logo" />
                 </a>
+                <div className="menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
                 <ul className={this.state.clicked ? "navMenu active" : "navMenu"}>
                     {menuNavbar.map((item, index) => {
                         return (
                             <li key={index}>
-                                <Link to={item.url} className="navlinks"> {/* Ensure className is consistent */}
+                                <Link to={item.url} className="navlinks">
                                     {item.title}
                                 </Link>
                             </li>
