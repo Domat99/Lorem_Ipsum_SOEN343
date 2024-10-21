3c085cd22ed52a967e2627157f72ce72b1ce2bc9
calinebatal <calinebatal01@gmail.com>
2024-10-21 01:19:13 -0400

    adding values to home page

--- react/myapp/src/HomePage/HeroSection.js
+++ react/myapp/src/HomePage/HeroSection.js
@@ -26,14 +26,17 @@ const HeroSection = () => {
                     <img src="/images/DeliveryMan.png" alt="Delivery Illustration"/>
                 </div>
             </div>
+
             <div className="values-container">
                 <h2 className="values-header">Why Pigeon Express is the best deal.</h2>
-                <p className="values-subheader">Conveniently ship, pick up or drop off your packages and leave the rest for us to take care of.</p>
+                <p className="values-subheader">Conveniently ship, pick up or drop off your packages and leave the rest
+                    for us to take care of.</p>
 
                 <div className="values">
                     <div className="values-item">
                         <i className="fa-solid fa-comments-dollar"></i>
-                        <p className="values-text"><b>No Hidden Fees</b><br/>Enjoy transparent pricing with no unexpected charges.
+                        <p className="values-text"><b>No Hidden Fees</b><br/>Enjoy transparent pricing with no
+                            unexpected charges.
                             What you see at checkout is exactly what you pay- no additional shipping charges.</p>
                     </div>
                     <div className="values-item">
@@ -44,11 +47,11 @@ const HeroSection = () => {
                     <div className="values-item">
                         <i className="fa-solid fa-hand-holding-heart"></i>
                         <p className="values-text"><b>Security</b><br/>Get extra peace of mind knowing we have
-                            advanced tracking and secure packaging, to ensure your items arrive in perfect condition.</p>
+                            advanced tracking and secure packaging, to ensure your items arrive in perfect condition.
+                        </p>
                     </div>
                 </div>
             </div>
-
 
         </div>
     );
