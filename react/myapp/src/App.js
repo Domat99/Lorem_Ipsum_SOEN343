4da7650d5b05f93fc027b7271680bc2f0e83b153
RawdaWaez <rawdawaez@gmail.com>
2024-11-05 17:52:16 -0500

    Implement the Footer.

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -1,22 +1,29 @@
 import React from "react";
 import Navbar from "./NavBar/NavBar";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Footer from "./Footer/Footer"; // Import the Footer component
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from "./HomePage/HomePage";
 import Shipping from "./Shipping/Shipping";
+// import Login from "./LogIn/LogIn";
+// import Register from "./RegisterForm/Register";
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
-  return (
-      <Router>
-        <div className="App">
-          <Navbar></Navbar>
-          <Routes>
-            <Route path="/" element={<HomePage/>} ></Route>
-              <Route path="/Ship" element={<Shipping/>} ></Route>
-          </Routes>
-        </div>
-      </Router>
-
-  );
+    return (
+        <Router>
+            <ScrollToTop /> {/* Add ScrollToTop here */}
+            <div className="App">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/Ship" element={<Shipping />} />
+                    {/*<Route path="/Login" element={<Login />} />*/}
+                    {/*<Route path="/Register" element={<Register />} />*/}
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
