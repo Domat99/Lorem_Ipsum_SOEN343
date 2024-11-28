d5ec16eba65ae3f66eb1f9342b6ecded9107c82d
Hazem-Horus <hazem.horus.7@gmail.com>
2024-11-28 00:23:38 -0500

    ChatBot commit 1

--- react/myapp/src/Footer/Footer.js
+++ react/myapp/src/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Footer.css';
+import ChatBot from '../AI-bot/AI-bot/AI-Assistant/app/route.js';
 
 const Footer = () => {
     return (
@@ -10,33 +11,34 @@ const Footer = () => {
                 <Link to="/FAQ">FAQ</Link>
                 <Link to='/'>Track Your Package</Link>
                 <Link to='/Policy'>Privacy Policy</Link>
-
             </div>
             <section className='social-media'>
                 <div className='social-media-wrap'>
                     <Link to='/' className='social-logo'>
-                        <img src="/Images/logo.png" alt="Pigeon Express Logo" className="footer-logo"/>
+                        <img src="/Images/logo.png" alt="Pigeon Express Logo" className="footer-logo" />
                     </Link>
                     <small className='website-rights'>Pigeon Express © {new Date().getFullYear()}</small>
                     <div className='social-icons'>
                         <Link className='social-icon-link facebook' to='/' target='_blank' aria-label='Facebook'>
-                            <i className='fab fa-facebook-f'/>
+                            <i className='fab fa-facebook-f' />
                         </Link>
                         <Link className='social-icon-link instagram' to='/' target='_blank' aria-label='Instagram'>
-                            <i className='fab fa-instagram'/>
+                            <i className='fab fa-instagram' />
                         </Link>
                         <Link className='social-icon-link youtube' to='/' target='_blank' aria-label='YouTube'>
-                            <i className='fab fa-youtube'/>
+                            <i className='fab fa-youtube' />
                         </Link>
                         <Link className='social-icon-link twitter' to='/' target='_blank' aria-label='Twitter'>
-                            <i className='fa-brands fa-x-twitter'/>
+                            <i className='fa-brands fa-x-twitter' />
                         </Link>
                         <Link className='social-icon-link linkedin' to='/' target='_blank' aria-label='LinkedIn'>
-                            <i className='fab fa-linkedin'/>
+                            <i className='fab fa-linkedin' />
                         </Link>
                     </div>
                 </div>
             </section>
+            {/* Add ChatBot Component */}
+            <ChatBot />
         </div>
     );
 }
